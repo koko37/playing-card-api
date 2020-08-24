77701a3899d8b9ee85a05160e00409929e7a743c
wjone <engel0088@gmail.com>
2020-08-24 23:09:17 +0900

    feat: config devise_token_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, 
-    :rememberable, :trackable, :validatable, :confirmable
+    :rememberable, :trackable, :validatable
+    # , :confirmable
   include DeviseTokenAuth::Concerns::User
 
   validates :username, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 2020_08_24_084720) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "username"
     t.string "email"
     t.text "tokens"
